b25ecbdfd4f5e7ca93247c66105a3f39f8b0fcb6
progbmy <skuter63@mail.ru>
2019-09-09 18:15:13 +0400

    Add Validates-user-posts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,8 @@
 class Micropost < ApplicationRecord
+#Делаем аасоциацию между пользователм и постом
+  belongs_to :user
+
+ # Делаем валидацию тоесть присваиваем полю контент опредленные значение максимум текст 140 исмволов
+  validates :content, length: { maximum: 140}, presence: true
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+#Делаем ассоциацию между пользователм и поставми, модель 1 пользователь и много постов
+  has_many :microposts
+  #Проверяем валидацию на наличие заполненных полей
+  validates :name, presence: true
+  validates :email, presence: true
+
+
 end
